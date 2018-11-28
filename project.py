0fcc811a211c6ba3fbffe2052b2db488e806be8c
giannisosx <giannis.osx@gmail.com>
2018-11-28 19:57:49 +0100

    roadNet

--- project.py
+++ project.py
@@ -16,25 +16,31 @@ import os
 import sys
 import matplotlib.pyplot as plt
 ###################################################
-os.chdir("/home/dead/Documents/SNACS/Snacs_Final")
+#os.chdir("/home/dead/Documents/SNACS/Snacs_Final")
+os.chdir("/home/melidell024/Desktop/snacs/project/Incremental-PageRank/")
 ####################################################
 from incremental import IncrementalPersonalizedPageRank2 as inc
 """
 Gnutella Fasoula
 Nodes 	6301
 Edges 	20777
+
+RoadsPA
+Nodes: 1088092
+Edges: 1541898
 """
 
 # Fixing the dataset format
-def preprocess(raw_data):
+def preprocess(raw_data,head=0):
     # Input: path of a raw dataset
     # Output: data_path of the corrected data
     # Export: Correct Dataset
+    
     os.chdir("datasets/")
     if not os.path.isfile('corrected'+raw_data):
         start = time()
-        pan = pd.read_csv(raw_data, sep="\t", header=None)
-        pan = pan.drop(0, axis=0)
+        pan = pd.read_csv(raw_data, sep="\t", header=head)
+        #pan = pan.drop(0, axis=0)
         pan.columns= ["Source", "Target"]
         data_path = 'corrected'+raw_data
         pan.to_csv(data_path, header=False, sep='\t', index = False )
@@ -51,12 +57,16 @@ def preprocess(raw_data):
 #data_path = preprocess('p2p-Gnutella08.txt')
 ####################################################
 
-def Import(datapath, discriptives=False):
+def Import(datapath, discriptives=False, directed=True):
     # Function for importing the dataset into networkx
     # Print some statistics
+    if directed:
+        graph_type = nx.DiGraph()
+    else:
+        graph_type = nx.Graph()
     start = time()
     with open('datasets/'+datapath, 'rb') as inf:
-        data = nx.read_edgelist(inf,create_using=nx.DiGraph(),\
+        data = nx.read_edgelist(inf,create_using=graph_type,\
                                 delimiter='\t', encoding="utf-8")
     print("Time taken for loading final.csv in secs: " + str(time()-start))
 
@@ -143,8 +153,8 @@ def Evaluate_retrieval(true,pred,plot=False):
 
 if __name__=='__main__':
     results=[]
-    data_path = preprocess('p2p-Gnutella08.txt')
-    data = Import(data_path, discriptives=True)
+    data_path = preprocess('roadNet-PA.txt',head = 4)
+    data = Import(data_path, discriptives=True, directed=False)
     t_nodes,t_values = PPR(data, node='4', sort=True, maxiter=500, alpha=0.7, num=20 )
     hat_nodes, hat_values = Approximate(data,node='4', sort=True, random=False, num=20 )
     results = Evaluate_values(t_values,hat_values)
